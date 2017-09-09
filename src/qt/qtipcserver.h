4eae75a30b4cfa70d651301c185a93d2407df119
loctai <mmo.hyipcent@gmail.com>
2017-09-09 08:26:12 +0700

    Freakcoin

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Freakcoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "FreakcoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
